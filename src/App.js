c577efdc3c7bdc88b7ed5208731edd3e02b73341
Federico <federicorg07@gmail.com>
2022-08-28 16:13:38 -0300

    skill section finish

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import About from "./components/About";
 import Home from "./components/Home";
 import Nabvar from "./components/Nabvar";
+import Skills from "./components/Skills";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Nabvar />
       <Home />
       <About />
+      <Skills />
     </div>
   );
 }
---
+++ src/components/Skills.js
@@ -0,0 +1,61 @@
+import React from 'react';
+
+import CSS from '../assets/css.png';
+import NODE from '../assets/node.png';
+import GIT from '../assets/git.png';
+import HTML from '../assets/html.png';
+import JAVASCRIPT from '../assets/javascript.png';
+import MYSQL from '../assets/mysql.png';
+import REACT from '../assets/react.png';
+import BOOTSTRAP from '../assets/boot.png';
+
+const Skills = () => {
+  return (
+    <div name='skills' className='w-full h-screen bg-[#0a192f] text-gray-300'>
+        <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+            <div>
+                <p className='text-4xl font-bold inline border-b-4 border-pink-600 '>Skills</p>
+                <p className='py-4'>Tecnologies I'have worked with</p>
+            </div>
+
+            <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-20 mx-auto' src={CSS} alt="logo node" />
+                    <p className='my-4'>CSS</p>
+                </div>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-20 mx-auto' src={NODE} alt="logo node" />
+                    <p className='my-4'>Node</p>
+                </div>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-20 mx-auto' src={GIT} alt="logo node" />
+                    <p className='my-4'>GITHUB</p>
+                </div>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-20 mx-auto' src={HTML} alt="logo node" />
+                    <p className='my-4'>HTML</p>
+                </div>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-20 mx-auto' src={JAVASCRIPT} alt="logo node" />
+                    <p className='my-4'>JAVASCRIPT</p>
+                </div>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-20 mx-auto' src={MYSQL} alt="logo node" />
+                    <p className='my-4'>MYSQL</p>
+                </div>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-20 mx-auto' src={REACT} alt="logo node" />
+                    <p className='my-4'>REACT</p>
+                </div>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-20 mx-auto' src={BOOTSTRAP} alt="logo node" />
+                    <p className='my-4'>BOOTSTRAP</p>
+                </div>
+            </div>
+        </div>
+
+    </div>
+  )
+}
+
+export default Skills
