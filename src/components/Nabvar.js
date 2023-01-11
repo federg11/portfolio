a7253ffdbb33adaf5c2d9c801c0a828091077ab2
federg <federicorg07@gmail.com>
2023-01-10 22:14:34 -0300

    changes added to home

--- src/components/Nabvar.js
+++ src/components/Nabvar.js
@@ -65,7 +65,7 @@ const Nabvar = () => {
               </li>
               <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
                 <a className='flex justify-between items-center w-full text-gray-300' 
-                href="/">
+                href="www.linkedin.com/in/federico-galup-a28791178">
                   Linkedin <FaLinkedin size={30} />
                 </a>
               </li>
@@ -77,7 +77,7 @@ const Nabvar = () => {
               </li>
               <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]'>
                 <a className='flex justify-between items-center w-full text-gray-300' 
-                href="/">
+                href="/Contact">
                   Email <HiOutlineMail size={30} />
                 </a>
               </li>
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -16,7 +16,7 @@ const Skills = () => {
       <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
           <div>
               <p className='text-4xl font-bold inline border-b-4 border-pink-600 '>Skills</p>
-              <p className='py-4'>These are the technologies I've worked with</p>
+              <p className='py-4'>Estas son las tecnologías con las que trabajé</p>
           </div>
 
           <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
@@ -46,11 +46,11 @@ const Skills = () => {
               </div>
               <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
                   <img className='w-20 mx-auto' src={BOOTSTRAP} alt="HTML icon" />
-                  <p className='my-4'>MONGO DB</p>
+                  <p className='my-4'>BOOTSTRAP</p>
               </div>
               <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
                   <img className='w-20 mx-auto' src={MYSQL} alt="HTML icon" />
-                  <p className='my-4'>AWS</p>
+                  <p className='my-4'>MYSQL</p>
               </div>
           </div>
       </div>
